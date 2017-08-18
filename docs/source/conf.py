ed4443f44fa9857d2f5ae449ed97ce4e3ddda4e2
chfw <wangc_2011@hotmail.com>
2017-08-19 00:25:10 +0100

    :egg: :ferris_wheels: release 0.0.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,8 +16,8 @@ master_doc = 'index'
 
 project = u'pyecharts-snapshot'
 copyright = u'2017 Onni Software Ltd.'
-version = '0.0.2'
-release = '0.0.2'
+version = '0.0.3'
+release = '0.0.3'
 exclude_patterns = []
 pygments_style = 'sphinx'
 html_theme = 'default'
