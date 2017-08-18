b294d3993102396d40fc3ccb7bfbecb9ff89acd0
chfw <wangc_2011@hotmail.com>
2017-08-18 23:54:59 +0100

    :sparkles: remove saveas image button from the final image

--- pyecharts_snapshot/phantomjs/snapshot.js
+++ pyecharts_snapshot/phantomjs/snapshot.js
@@ -2,10 +2,16 @@ var page = require('webpage').create();
 var system = require('system');
 
 var file = system.args[1];
+var snapshot = "" + 
+"    function(){"+
+"        var ele = document.querySelector('div[_echarts_instance_]');"+
+"        var mychart = echarts.getInstanceByDom(ele);"+
+"        return mychart.getDataURL({type:'png', excludeComponents: ['toolbox']});"+
+"    }";
 
 page.open(file, function(){
 	window.setTimeout(function () {
-		var content = page.evaluateJavaScript("function(){var ele = document.querySelector('div[_echarts_instance_]'); var mychart = echarts.getInstanceByDom(ele); return mychart.getDataURL({type:'png'});}");
+		var content = page.evaluateJavaScript(snapshot);
 		console.log(content);
         phantom.exit();
     }, 1000);
