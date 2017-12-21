29281766e45f0e4610ee1e0050ba0b29bf0eabdd
chfw <wangc_2011@hotmail.com>
2017-12-21 20:49:13 +0000

    :egg: :ferris_wheel: release 0.1.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ PY26 = PY2 and sys.version_info[1] < 7
 
 NAME = 'pyecharts-snapshot'
 AUTHOR = 'C.W.'
-VERSION = '0.1.1'
+VERSION = '0.1.2'
 EMAIL = 'wangc_2011@hotmail.com'
 LICENSE = 'MIT'
 ENTRY_POINTS = {
@@ -21,8 +21,8 @@ DESCRIPTION = (
     'renders pyecharts as image' +
     ''
 )
-URL = 'https://github.com/chfw/pyecharts-snapshot'
-DOWNLOAD_URL = '%s/archive/0.1.1.tar.gz' % URL
+URL = 'https://github.com/pyecharts/pyecharts-snapshot'
+DOWNLOAD_URL = '%s/archive/0.1.2.tar.gz' % URL
 FILES = ['README.rst',  'CONTRIBUTORS.rst', 'CHANGELOG.rst']
 KEYWORDS = [
     'echarts',
@@ -60,8 +60,8 @@ EXTRAS_REQUIRE = {
 # You do not need to read beyond this line
 PUBLISH_COMMAND = '{0} setup.py sdist bdist_wheel upload -r pypi'.format(
     sys.executable)
-GS_COMMAND = ('gs pyecharts-snapshot v0.1.1 ' +
-              "Find 0.1.1 in changelog for more details")
+GS_COMMAND = ('gs pyecharts-snapshot v0.1.2 ' +
+              "Find 0.1.2 in changelog for more details")
 NO_GS_MESSAGE = ('Automatic github release is disabled. ' +
                  'Please install gease to enable it.')
 UPLOAD_FAILED_MSG = (
