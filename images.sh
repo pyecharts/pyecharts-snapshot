68169e2ac47ebf3091a4f1378e9542c5faab95b7
chfw <wangc_2011@hotmail.com>
2018-02-05 07:57:26 +0000

    :books: update examples with extra delay to provide full screenshot

--- images.sh
+++ images.sh
@@ -5,5 +5,5 @@ mv output.png ../images/demo.png
 python bar.py
 mv snapshot.png ../images/snapshot.png
 python pdf.py
-snapshot render.html pdf
+snapshot render.html pdf 5
 mv output.pdf snapshot_in_pdf.pdf
