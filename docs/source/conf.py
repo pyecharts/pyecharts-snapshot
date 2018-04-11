dc5d805976174719a887031b7e5596a1817a0597
chfw <wangc_2011@hotmail.com>
2018-04-11 09:36:47 +0100

    release 0.1.5 with better error output :egg: :ferris_wheel:

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ master_doc = 'index'
 
 project = u'pyecharts-snapshot'
 copyright = u'2017-2018 Onni Software Ltd.'
-version = '0.1.4'
+version = '0.1.5'
 release = '0.1.5'
 exclude_patterns = []
 pygments_style = 'sphinx'
