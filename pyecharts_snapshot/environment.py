ff247f71e59eb45eb097c156f5c4057e25815fec
chfw <wangc_2011@hotmail.com>
2018-05-16 20:23:58 +0100

    :hammer: use temp file in system temp folder

--- pyecharts_snapshot/environment.py
+++ pyecharts_snapshot/environment.py
@@ -1,5 +1,6 @@
 import os
 import codecs
+from tempfile import mkstemp
 
 from pyecharts.engine import EchartsEnvironment
 
@@ -23,15 +24,16 @@ class SnapshotEnvironment(EchartsEnvironment):
             delay=DEFAULT_DELAY,
             **kwargs):
         _, extension = os.path.splitext(path)
+        tmp_file_handle, tmp_file_path = mkstemp()
         super(SnapshotEnvironment, self).render_chart_to_file(
             chart=chart,
             object_name=object_name,
-            path='tmp.html',
+            path=tmp_file_path,
             template_name=template_name,
             **kwargs
         )
-        make_a_snapshot('tmp.html', path, delay=delay, verbose=verbose)
-        os.unlink('tmp.html')
+        make_a_snapshot(tmp_file_path, path, delay=delay, verbose=verbose)
+        os.close(tmp_file_handle)
         content = None
         if extension == '.svg':
             with codecs.open(path, 'r', 'utf-8') as f:
