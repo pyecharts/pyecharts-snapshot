3ee9ff9429f0bba6f1586a1a82b89978ac2e8f18
chfw <wangc_2011@hotmail.com>
2018-05-31 22:30:06 +0100

    :egg: :ferris_wheel: release 0.1.7

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,8 +16,8 @@ master_doc = 'index'
 
 project = u'pyecharts-snapshot'
 copyright = u'2017-2018 Onni Software Ltd.'
-version = '0.1.6'
-release = '0.1.6'
+version = '0.1.7'
+release = '0.1.7'
 exclude_patterns = []
 pygments_style = 'sphinx'
 html_theme = 'default'
--- setup.py
+++ setup.py
@@ -11,21 +11,19 @@ PY26 = PY2 and sys.version_info[1] < 7
 
 NAME = 'pyecharts-snapshot'
 AUTHOR = 'C.W.'
-VERSION = '0.1.6'
+VERSION = '0.1.7'
 EMAIL = 'wangc_2011@hotmail.com'
 LICENSE = 'MIT'
 ENTRY_POINTS = {
     'console_scripts': [
         'snapshot = pyecharts_snapshot.main:main'
-    ]
+    ],
 }
 DESCRIPTION = (
     'renders pyecharts output as image'
 )
-
 URL = 'https://github.com/pyecharts/pyecharts-snapshot'
-
-DOWNLOAD_URL = '%s/archive/0.1.6.tar.gz' % URL
+DOWNLOAD_URL = '%s/archive/0.1.7.tar.gz' % URL
 FILES = ['README.rst', 'CONTRIBUTORS.rst', 'CHANGELOG.rst']
 KEYWORDS = [
     'echarts',
@@ -64,8 +62,8 @@ EXTRAS_REQUIRE = {
 # You do not need to read beyond this line
 PUBLISH_COMMAND = '{0} setup.py sdist bdist_wheel upload -r pypi'.format(
     sys.executable)
-GS_COMMAND = ('gs pyecharts-snapshot v0.1.6 ' +
-              "Find 0.1.6 in changelog for more details")
+GS_COMMAND = ('gs pyecharts-snapshot v0.1.7 ' +
+              "Find 0.1.7 in changelog for more details")
 NO_GS_MESSAGE = ('Automatic github release is disabled. ' +
                  'Please install gease to enable it.')
 UPLOAD_FAILED_MSG = (
