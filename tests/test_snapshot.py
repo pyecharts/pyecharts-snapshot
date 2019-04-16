653b1206e15a7404ad977abf199a7b1e1801c91e
chfw <wangc_2011@hotmail.com>
2019-04-16 23:54:21 +0100

    :green_heart: fix merge error

--- tests/test_snapshot.py
+++ tests/test_snapshot.py
@@ -12,7 +12,7 @@ from pyecharts_snapshot.main import (
     make_a_snapshot,
 )
 
-from mock import patch
+from mock import patch, Mock
 from nose.tools import eq_, raises
 from aiounittest import async_test
 
